8ccd61a61067b9e678e42e9cc72bf11b35f1400b
Rxyanlaw <a4804260513@gmail.com>
2020-10-13 13:26:23 -0400

    Update AntiCheat.lua

--- AntiCheat.lua
+++ AntiCheat.lua
@@ -1,1 +1,4 @@
-
+if game.Players.Humaniod.Walkspeed ==16 then
+  wait()
+  game.Players.FindFirstChild(..Player.Name..):Kick("Unusual Player Behavior Detected. Applied ban for 1220 minutes")
+  end
